dd8a65b451e63b7b8c907a3e9555ea8ed815f647
Igor Tarasov <xromer85@gmail.com>
2021-11-13 21:04:42 +0300

    function

---
+++ src/js/fetchCountries.js
@@ -0,0 +1,15 @@
+//fetchCountries(name);
+const END_POINT = 'https://restcountries.com/v3.1/name/';
+const name = 'ukr';
+const FILTER = '?fields=name,capital,population,flags,languages';
+
+console.log(fetchCountries(name));
+
+export default function fetchCountries(name) {
+  fetch(`${END_POINT}${name}${FILTER}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
